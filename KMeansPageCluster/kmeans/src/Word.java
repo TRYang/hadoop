450ee2ae4d3b7373f60fa2253b24040e1701310b
TRYang <ruiyangtan.terry@gmail.com>
2012-12-18 20:37:01 +0800

    update PageCluster

---
+++ KMeansPageCluster/kmeans/src/Word.java
@@ -0,0 +1,27 @@
+	
+public class Word implements Comparable<Word> {
+			
+	Word() {
+		tfidf = 0;
+		word = "";
+	}
+	
+	Word(String str) {
+		String [] tm = str.split(" ");
+		tfidf = Double.parseDouble(tm[0]);
+		word = new String(tm[1]);
+	}
+	
+	Word(String a, double b) {
+		word = a;
+		tfidf = b;
+	}
+		
+	public int compareTo(Word b) {
+		if (Math.abs(this.tfidf - b.tfidf) < 1e-12) return 0;
+		return this.tfidf > b.tfidf ? 1 : -1;
+	}
+		
+	double tfidf;
+	String word;
+}
